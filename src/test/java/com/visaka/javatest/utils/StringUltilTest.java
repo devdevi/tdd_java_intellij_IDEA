1e69b227ae2ce397fc5a4116375507502684c297
visidevi <32283668+visidevi@users.noreply.github.com>
2019-11-03 19:17:21 -0300

    trow exception'

--- src/test/java/com/visaka/javatest/utils/StringUltilTest.java
+++ src/test/java/com/visaka/javatest/utils/StringUltilTest.java
@@ -7,15 +7,21 @@ public class StringUltilTest {
     public static void main(String[] args) {
 
         String result = StringUltil.repeat( "Hola", 3 );
+        assertEquals(result, "HolaHolaHola");
 
-        if (result.equals("HolaHolaHola")){
-            System.out.println("OK");
+
+        String result2 = StringUltil.repeat( "Hola", 1 );
+        assertEquals(result2, "Hola");
+//        if (result2.equals("HolaHol")){
+////            System.out.println("ERROR");
+//            throw new RuntimeException("ERROR");
+//        }
+
+    }
+
+    private static void assertEquals(String actual, String expected) {
+        if (!actual.equals(expected)){
+            throw new RuntimeException(actual + " is not equal to expected " + expected);
         }
-
-        String result2 = StringUltil.repeat( "Hola", 2 );
-        if (!result2.equals("HolaHol")){
-            System.out.println("ERROR");
-        }
-
     }
 }
