eeb2a63bb13e9846e7f7c2e9a5500b63f798adf1
visidevi <32283668+visidevi@users.noreply.github.com>
2019-11-03 23:05:49 -0300

    setup en test

--- src/main/java/com/visaka/javatest/payments/PaymentProcessor.java
+++ src/main/java/com/visaka/javatest/payments/PaymentProcessor.java
@@ -3,14 +3,14 @@ package com.visaka.javatest.payments;
 public class PaymentProcessor {
 
     private PaymentGateway paymentGateway;
-
+//  Constructor
     public PaymentProcessor(PaymentGateway paymentGateway) {
         this.paymentGateway = paymentGateway;
     }
-
+//  Metodos
     public boolean makePayment(double amount) {
 
-//        llamar Gateway
+//      llamar Gateway o interface
         PaymentResponse response = paymentGateway.requestPayment(new PaymentRequest(amount));
 
         if (response.getStatus() == PaymentResponse.PaymentStatus.OK) {
--- src/test/java/com/visaka/javatest/payments/PaymentProcessorTest.java
+++ src/test/java/com/visaka/javatest/payments/PaymentProcessorTest.java
@@ -7,29 +7,43 @@ import static org.junit.Assert.*;
 
 public class PaymentProcessorTest {
 
+//   METODO COMUN PARA EVITAR REPETIR CODIGO
+//    public void setup() {
+//        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
+//        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
+//
+//    }
+
     @Test
     public void payment_is_correct() {
 
 //        Simulamos el paymentGateway
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
+//      1.PREPARACION DEL ESCENARIO QUE VAMOS A USAR EN EL TEST
 
+        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.requestPayment(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
         PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
-        assertTrue(paymentProcessor.makePayment(1000));
+//       2. LLAMADA AL METODO QUE QUEREMOS PROBAR
+        boolean result = paymentProcessor.makePayment(1000);
+
+//      3. ==== RESULTADO ESPERADO
+        assertTrue(result);
     }
 
     @Test
     public void payment_is_wrong() {
 
-//        Simulamos el paymentGateway
+//      1. PREPARACION DEL METODO
         PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
 
         Mockito.when(paymentGateway.requestPayment(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
         PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
+//      2. EJECT DEL METODO
+//      3. CHECK DEL RESULTADO
         assertFalse(paymentProcessor.makePayment(1000));
     }
 }
