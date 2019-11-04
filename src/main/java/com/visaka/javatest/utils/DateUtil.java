ca95fa4e1195054fdc2957b1c9127dda2c313bde
visidevi <32283668+visidevi@users.noreply.github.com>
2019-11-04 00:42:47 -0300

    tdd bisiesto leapYear

---
+++ src/main/java/com/visaka/javatest/utils/DateUtil.java
@@ -0,0 +1,22 @@
+package com.visaka.javatest.utils;
+
+public class DateUtil {
+
+
+
+//    Funcion que devuelve si el año es biciesto;
+    public static  boolean isLeapYear(int year) {
+
+        if (year % 4 == 0 && year % 100 != 0) {
+
+            return  true;
+        }
+
+        if (year % 400 == 0) {
+            return  true;
+        } else {
+            return false;
+        }
+
+    }
+}
