589fcb9b1fe4a7f47d1485cff03c6f45737b069c
visidevi <32283668+visidevi@users.noreply.github.com>
2019-11-04 00:45:20 -0300

    refactor

--- src/main/java/com/visaka/javatest/utils/DateUtil.java
+++ src/main/java/com/visaka/javatest/utils/DateUtil.java
@@ -7,16 +7,19 @@ public class DateUtil {
 //    Funcion que devuelve si el año es biciesto;
     public static  boolean isLeapYear(int year) {
 
-        if (year % 4 == 0 && year % 100 != 0) {
+//        if (year % 4 == 0 && year % 100 != 0 ) {
+//
+//            return  true;
+//        }
+//
+//        if (year % 400 == 0) {
+//            return  true;
+//        } else {
+//            return false;
+//        }
 
-            return  true;
-        }
-
-        if (year % 400 == 0) {
-            return  true;
-        } else {
-            return false;
-        }
+//        REFACTOR
+        return  ((year % 4 == 0 && year % 100 != 0 ) || (year % 400 == 0));
 
     }
 }
