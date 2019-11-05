a35a0ecdc172b5f683f15bde1cdcb9055bd42c21
visidevi <32283668+visidevi@users.noreply.github.com>
2019-11-04 23:10:16 -0300

    discount

---
+++ src/main/java/com/visaka/javatest/discounts/PriceCalculator.java
@@ -0,0 +1,22 @@
+package com.visaka.javatest.discounts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PriceCalculator {
+
+    private List<Double> prices = new ArrayList<>();
+
+    public double getTotal() {
+
+        double result = 0;
+        for (Double price : prices) {
+            result += price;
+        }
+        return result;
+    }
+
+    public void addPrice(double price) {
+        prices.add(price);
+    }
+}
---
+++ src/test/java/com/visaka/javatest/discounts/PriceCalculatorShould.java
@@ -0,0 +1,28 @@
+package com.visaka.javatest.discounts;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+public class PriceCalculatorShould {
+
+    //        instancia de clase
+    private  PriceCalculator calculator = new PriceCalculator();
+
+    @Test
+    public  void  total_zero_when_there_are_price() {
+        assertThat(calculator.getTotal(), is(0.0));
+    }
+    @Test
+    public void total_is_the_sum_of_price() {
+
+        calculator.addPrice(10.0);
+        calculator.addPrice(15.0);
+
+        assertThat(calculator.getTotal(), is(25.0));
+    }
+
+}
